f7315adb79b0ab56dd93c528a4d2dedc1a57ae2c
himi513 <hokada513@gmail.com>
2023-10-07 10:01:21 +0800

    deviseのルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'items/index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
