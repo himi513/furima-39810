dd7c7914bec4879839633bc46430151299518ccf
himi513 <hokada513@gmail.com>
2023-10-10 22:36:43 +0900

    item validationの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,7 +25,5 @@ class Item < ApplicationRecord
   end
 
   validates :price, numericality: { message: "には半角数字を使用してください" }
-  validates :price, presence: true,
-            numericality: { only_integer: ture, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
