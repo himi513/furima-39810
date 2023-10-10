581eb2dff068af7179756ef0ad69be781bf3e590
himi513 <hokada513@gmail.com>
2023-10-10 17:11:04 +0900

    各モデル設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category_id
+  belongs_to :situation_id
+  belongs_to :delivery_charge_id
+  belongs_to :region_id
+  belongs_to :shipment_id
+
+  belongs_to :user
+
 end
