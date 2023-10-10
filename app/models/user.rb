65588d2efa38cb70e1a6a2aae971b7fb1053d9fe
himi513 <hokada513@gmail.com>
2023-10-10 16:40:53 +0900

    Validationの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birth_date, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ--龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ-]+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
