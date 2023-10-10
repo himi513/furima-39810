5dfe7d7d621a08aa07c9775863c275de62be8c8e
himi513 <hokada513@gmail.com>
2023-10-10 20:54:20 +0900

    データの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprosessable_entity
+    end
   end 
 
   private
