723bbe2cdd6c4bf9ba6d945a087a4ddf880137b6
himi513 <hokada513@gmail.com>
2023-10-12 12:08:48 +0900

    決済機能

--- spec/factories/purchase_records.rb
+++ spec/factories/purchase_records.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :purchase_record do
-    post_code   { '123-4567' }
-    region_id   { Faker::Number.between(from: 2, to: 48) }
-    city        { Faker::Address.city }
-    street      { '1-1' }
-    building    { '東京ハイツ' }
-    telephone   { Faker::Number.decimal_part(digits: 11) } 
+    
   end
 end
