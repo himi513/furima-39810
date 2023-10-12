ca69c923d559887699d87aca7c9f6c1c6472c6a5
himi513 <hokada513@gmail.com>
2023-10-12 11:13:53 +0900

    テストコード

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -1,18 +1,19 @@
 class PurchaseRecordAddress
   include ActiveModel::Model
-  attr_accessor :item_id,:user_id, :region_id, :post_cord, :city, :street, :building, :telephone
+  attr_accessor :item_id, :user_id, :region_id, :post_code, :city, :street, :building, :telephone
 
   with_options presence: true do
-    validates :item_id, :user_id
-    validates :city, :street
+    validates :item_id
+    validates :user_id
+    validates :city
+    validates :street
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :telephone, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
+    validates :telephone, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid"}
+    validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
   end
 
-  validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
-
   def save
-    purchase_record = PurchaseRecord.create(item_id: params, user_id: user_id)
+    purchase_record = PurchaseRecord.create(item_id: item_id, user_id: user_id)
     Address.create(post_code: post_code, region_id: region_id, city: city, street: street, building: building, telephone: telephone, purchase_record_id: purchase_record.id)
   end
 end
--- db/migrate/20231011120201_create_addresses.rb
+++ db/migrate/20231011120201_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
       t.integer :region_id,            null: false
-      t.string :post_cord,             null: false
+      t.string :post_code,             null: false
       t.string :city,                  null: false
       t.string :street,                null: false
       t.string :building
